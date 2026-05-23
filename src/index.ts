c7c53e2a48f4352d7d3fc16d76b3bb37294b56dd
Maxime Vincent <git@maximevincent.com>
2026-05-22 21:19:15 -0400

    use bun router

--- src/index.ts
+++ src/index.ts
@@ -9,36 +9,38 @@ console.log(config);
 const server = Bun.serve({
   hostname: config.server.host,
   port: config.server.port,
+  routes: {
+    "/status": new Response("OK", { status: 200 }),
+    "/webhooks/roller": {
+      POST: async (req) => {
+        const url = new URL(req.url);
+        const secret = url.searchParams.get("secret");
 
-  async fetch(req) {
-    const url = new URL(req.url);
-    if (url.pathname === "/status") {
-      return new Response("OK", { status: 200 });
-    }
+        if (secret !== Bun.env.ROLLER_WEBHOOK_SECRET) {
+          customLog(`Unauthorized webhook access attempt`, "WARN");
+          return new Response("Unauthorized", { status: 401 });
+        }
 
-    if (url.pathname === "/webhooks/roller" && req.method === "POST") {
-      const secret = url.searchParams.get("secret");
-      if (secret !== Bun.env.ROLLER_WEBHOOK_SECRET) {
-        customLog(`Unauthorized webhook access attempt`, "WARN");
-        return new Response("Unauthorized", { status: 401 });
-      }
+        let payload: any;
+        try {
+          payload = await req.json();
+        } catch {
+          customLog(`Rejected webhook, invalid JSON payload`, "ERROR");
+          return new Response("Bad Request", { status: 400 });
+        }
 
-      let payload: any;
-      try {
-        payload = await req.json();
-      } catch {
-        customLog(`Rejected webhook, invalid JSON payload`, "ERROR");
-        return new Response("Bad Request", { status: 400 });
-      }
+        // Todo : Ajouter l'ID et le type de request dans le log
+        customLog(`Received webhook from ROLLER with ID: id and type: type`);
 
-      // Todo : Ajouter l'ID et le type de request dans le log
-      customLog(`Received webhook from ROLLER with ID: id and type: type`);
+        await handleWebhook(payload);
 
-      await handleWebhook(payload);
+        return new Response("OK", { status: 200 });
+      },
+    },
+  },
 
-      return new Response("OK", { status: 200 });
-    }
-
+  // 3. Fallback: Handles any requests that don't match the `routes` object.
+  fetch(req) {
     return new Response("Not Found", { status: 404 });
   },
 });
