309a3bf6c697793e1c01f9a35a304115283a4ea8
KentCShit <94623626+RaphBarniques@users.noreply.github.com>
2026-06-24 18:02:58 -0400

    exclude uptime request from log

--- src/middleware/req_logging.ts
+++ src/middleware/req_logging.ts
@@ -5,12 +5,15 @@ async function logging(req: Request, next: Handler): Promise<Response> {
 	const start = Date.now();
 	const ip = req.headers.get('X-Forwarded-For') ?? 'unknown';
 	const ua = req.headers.get('User-Agent') ?? 'unknown';
+	const isUptimeRobot = ua.toLowerCase().includes('uptimerobot.com');
 
 	const res = await next(req);
 
-	customLog(
-		`${req.method} ${req.url} ${res.status} ${Date.now() - start}ms ip=${ip} ua=${ua}`,
-	);
+	if (!isUptimeRobot) {
+		customLog(
+			`${req.method} ${req.url} ${res.status} ${Date.now() - start}ms ip=${ip} ua=${ua}`,
+		);
+	}
 	return res;
 }
 
